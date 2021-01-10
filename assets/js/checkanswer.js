b7a9b705c8fce05794dc3512cc6d4bc8c86e61e6
S-Lowes <seblowes@btinternet.com>
2021-01-10 16:49:58 +0000

    trying to get string-similarity to work alongside the form in js

--- assets/js/checkanswer.js
+++ assets/js/checkanswer.js
@@ -1,4 +1,7 @@
 function checkAnswer() {
     // send `submit` request to server
+    // document.getElementById("userAnswer").value = userAnswer;
+    // document.getElementById("answer").innerHTML = realAnswer;
+    // console.log(stringSimilarity.compareTwoStrings(userAnswer,realAnswer));
     console.log("hello");
 }
