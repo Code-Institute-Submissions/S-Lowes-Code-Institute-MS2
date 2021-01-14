cb39b8ce13a67f9f02755a419d47eea58d87bf62
S-Lowes <seblowes@btinternet.com>
2021-01-14 18:23:41 +0000

    Category buttons now fetch the data from the api for the chosen topic

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,32 +1,49 @@
 let score = 0; //Create variable which we will use to track the score
-let cat=0; //Create variable cat, which will be used when fetching the category we want questions from.
+let cat = 0;
+const catA = document.getElementById("catA");
+const catB = document.getElementById("catB");
+const catC = document.getElementById("catC");
+const catD = document.getElementById("catD");
+const CatE = document.getElementById("catE");
+const catF = document.getElementById("catF");
+
+// Choose a Category - Grab Category from jService
+
+function category(a,b) { //Fetch taken from "https://developers.google.com/web/updates/2015/03/introduction-to-fetch"
+    console.log(a);
+    cat=b;
+    console.log(cat);
+
+    var c = cat.toString();
+
+    fetch('https://jservice.io/api/category?id='+c) //Accessing the jService API - https worked, http didn't
+    .then(
+      function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data)
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+}
 
 //Event listener for category choice
 
-catA.addEventListener('click', function(){
-    console.log("CatA")
-    let cat=21;
-});
-catB.addEventListener('click', function(){
-    console.log("CatB")
-    let cat=114;
-});
-catC.addEventListener('click', function(){
-    console.log("CatC")
-    let cat=680;
-});
-catD.addEventListener('click', function(){
-    console.log("CatD")
-    let cat=25;
-});
-catE.addEventListener('click', function(){
-    console.log("CatE")
-    let cat=42;
-});
-catF.addEventListener('click', function(){
-    console.log("CatF")
-    let cat=227;
-});
+catA.addEventListener('click', () => category("CatA",21));
+catB.addEventListener('click', () => category("CatB",114));
+catC.addEventListener('click', () => category("CatC",680));
+catD.addEventListener('click', () => category("CatD",25));
+catE.addEventListener('click', () => category("CatE",42));
+catF.addEventListener('click', () => category("CatF",227));
 
 //Checking the Answer
 
@@ -42,31 +59,3 @@ function checkAnswer() {
     console.log(score)
     document.getElementById("userAnswer").value="";
 }
-
-//const btnNext = document.getElementById("btnNext"); (Do we need this?)
-
-btnNext.addEventListener('click', function(){ //Fetch taken from "https://developers.google.com/web/updates/2015/03/introduction-to-fetch"
-    fetch('https://jservice.io/api/random') //Accessing the jService API - https worked, http didn't
-    .then(
-      function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        const question = data[0].question; //Using data[0] to go down a level and then ask for the question specifically.
-        const answer = data[0].answer;
-        //console.log(`${question}: ${answer}`)
-        document.getElementById("question").innerHTML = question;
-        document.getElementById("answer").innerHTML = answer;
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-
-});
